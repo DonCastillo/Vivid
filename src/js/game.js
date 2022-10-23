54effdb03f48274dfb858f9c7e2d0e9f8fec9236
Don Castillo <don@tangle.ca>
2022-10-22 19:43:13 -0600

    initial files

---
+++ src/js/game.js
@@ -0,0 +1,10 @@
+import 'jquery-sortablejs';
+import jQuery from 'jquery';
+const $ = jQuery;
+
+
+$(document).ready(function(){
+    // alert('Hello');
+    console.log('working jquery');
+    $('.block-strong').css('backgroundColor', 'green')
+});
