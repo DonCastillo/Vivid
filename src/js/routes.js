e437b048db36f8a33e622644f2bda0fcb5de6dbd
Don Castillo <don@tangle.ca>
2022-10-22 20:29:55 -0600

    added about page

--- src/js/routes.js
+++ src/js/routes.js
@@ -1,11 +1,16 @@
 
 import HomePage from '../pages/home.f7';
+import AboutPage from '../pages/about.f7';
 
 var routes = [
   {
     path: '/',
     component: HomePage,
   },
+  {
+    path: '/about',
+    component: AboutPage
+  }
 ];
 
 export default routes;
